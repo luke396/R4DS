a6f047ce37e126b89fb7f5e16f8d0fe3b2dcb233
luke396 <77875500+luke396@users.noreply.github.com>
2024-02-05 21:06:02 +0800

    Complete 1.4

--- C1.R
+++ C1.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(palmerpenguins)
 library(ggthemes)
 
+# C1.2
 ggplot(
   data = penguins,
   mapping = aes(x = flipper_length_mm, y = body_mass_g)
@@ -117,3 +118,52 @@ ggplot() +
 # The two plots are the same. The first plot is set by `ggplot()` - global level, it will apply for all following layers.
 # The second plot is set by `geom_point()` and `geom_smooth()` - local level, it will apply for only these layers.
 # But, in the second plot, the `data` and `mapping` are set for each layer, so the two plots are the same.
+
+# C1.4
+# When variable's hist chat is too difficult to determine the `binwidth`,we draw density (a smoothed-out version of a histogram, especially for continues data comes from an underlying smooth distribution) plot instead.
+
+# Exercise 1
+# Use `y = species` will draw a bar plot with horizontal bars.
+ggplot(
+  data = penguins,
+  mapping = aes(y = species)
+) +
+  geom_bar()
+
+# Exercise 2
+# `color` is used for the border of the bars,
+# and `fill` is used for the inside of the bars.
+ggplot(penguins, aes(x = species)) +
+  geom_bar(color = "red")
+
+ggplot(penguins, aes(x = species)) +
+  geom_bar(fill = "red")
+
+# Exercise 3
+# Note the difference between `geom_histogram` and `geom_bar`.
+# `geom_histogram` is used for continues data, and `geom_bar` is used for discrete data.
+# But, in my view, they are the same especially for continues data
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram()
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_bar()
+# argument `bins` used to set the number of bins in the histogram.
+# it's another way to get the same effect of `binwidth` in `geom_histogram`.
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(bins = 30)
+
+# Exercise 4
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 20)
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 5)
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 1)
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 0.5)
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 0.1)
+ggplot(diamonds, aes(x = carat)) +
+  geom_histogram(binwidth = 0.005)
+# The different `binwidth` will lead to different number of bins in the histogram.
+# The interesting pattern is that the `carat` peaks and troughs with a certain period, more significant when `binwidth` is smaller.
