6470439e567002d05098a3911b299dbf71040ce1
luke396 <77875500+luke396@users.noreply.github.com>
2024-02-07 20:20:56 +0800

    Complete C1.5

--- C1.R
+++ C1.R
@@ -167,3 +167,99 @@ ggplot(diamonds, aes(x = carat)) +
   geom_histogram(binwidth = 0.005)
 # The different `binwidth` will lead to different number of bins in the histogram.
 # The interesting pattern is that the `carat` peaks and troughs with a certain period, more significant when `binwidth` is smaller.
+
+
+# C1.5
+# numerical vs categorical - boxplot
+ggplot(penguins, aes(x = body_mass_g, color = species, fill = species)) +
+  geom_density(alpha = 0.5)
+
+# categoral vs categoral
+# `position = "fill"` to compare the proportion of each species on each island.
+ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar(position = "fill")
+
+# numerical vs numerical
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(aes(color = species, shape = species)) +
+  facet_wrap(~island) # create a separate plot for each island
+
+# Exercise 1
+# ?mpg
+view(mpg)
+# check the type of variable in `mpg`
+# Loop through all variables in the mpg dataset
+for (col_name in names(mpg)) {
+  # Get the type of each variable and print it
+  cat("Variable:", col_name, "\tType:", class(mpg[[col_name]]), "\n")
+}
+
+# Exercise 2
+# displ - engine displacement (liters)
+# hwy - highway miles per gallon
+# cyl - number of cylinders
+# drv - the type of drive train
+# class - the type of car
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = year))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(size = cyl))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(size = cyl, color = year))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(shape = drv))
+
+ggplot(mpg, aes(y = displ, x = hwy)) +
+  geom_point(aes(shape = drv, color = year, size = cyl)) +
+  facet_wrap(~class) +
+  labs(title = "Highway miles per gallon by engine displacement") +
+  theme_minimal()
+
+# Exercise 3
+ggplot(mpg, aes(y = hwy, x = displ)) +
+  geom_point(aes(linewitdth = cyl))
+# There is no `linewitdth` in `aes` of `geom_point` - scatter plot.
+
+# Exercise 4
+# If I am not misunderstanding, when using same variable in `aes` of `ggplot` and `geom_point`, it's also the problem of local and global level.
+# For this case, the global level is set by `ggplot` and should be shown in the plot.
+ggplot(mpg, aes(y = hwy, x = displ)) +
+  geom_point(aes(x = displ, y = hwy))
+
+# Exercise 5
+ggplot(penguins, aes(y = bill_depth_mm, x = bill_length_mm)) +
+  geom_point(aes(color = species))
+# The plot shows that the relationship between bill length and bill depth is positive linear, and different species have different bill length and bill depth(can be divided by color).
+
+# Exercise 6
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = bill_length_mm, y = bill_depth_mm,
+    color = species, shape = species
+  )
+) +
+  geom_point() +
+  labs(color = "Species")
+# The reason why chat above show two legend is `labs(color = "Species")`. Below is the correct code:
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = bill_length_mm, y = bill_depth_mm,
+    color = species, shape = species
+  )
+) +
+  geom_point()
+
+# Exercise 7
+ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar(position = "fill")
+
+ggplot(penguins, aes(x = species, fill = island)) +
+  geom_bar(position = "fill")
+
+# The two chats above is concerning the dependent variable and independent variable.
+# The first chat is the proportion of each species on each island, and the second chat is the proportion of each island for each species.
